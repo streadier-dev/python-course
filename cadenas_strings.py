341d591d4fca59849286c814dc1a12c5e3638161
gabriel <gabriel.faure@readinessit.com>
2020-02-14 17:10:45 -0300

    cadenas de strings

---
+++ cadenas_strings.py
@@ -0,0 +1,18 @@
+
+my_string = 'platzi'
+
+resultado_length = my_string[len(my_string) - 5]
+
+resultado_upper = my_string.upper()
+
+resultado_lower = my_string.lower()
+
+resultado_find = my_string.find('z')
+
+print(resultado_upper)
+print(resultado_length)
+print(resultado_lower)
+print(resultado_find)
+#print(my_string[len(my_string) - 1])
+
+#print(my_string[0],my_string[1])
