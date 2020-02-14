3370378beafcc7b71e8aa1166c51ad8a07b23acb
gabriel <gabriel.faure@readinessit.com>
2020-02-14 17:27:36 -0300

    cadenas_separar_textos

---
+++ cadenas_separar_texto.py
@@ -0,0 +1,11 @@
+
+my_string = 'platzi'
+
+inicio = 1
+final = 6
+saltos = 2
+
+print(my_string[1], my_string[1:], my_string[0:5],
+      my_string[1:6:2], my_string[::-1], my_string[::1])
+
+print (my_string[inicio:final:saltos])
